3362496edbd00f0dd3d2190844b3af13551986d2
arindra-sera <muhammad.arindra@sera.astra.co.id>
2023-03-02 09:55:08 +0700

    feat: learn nodejs native

--- server.js
+++ server.js
@@ -1,44 +1,50 @@
 const http = require('http')
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    // response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'appilcation/json');
+    response.setHeader('X-Powered-By', 'NodeJs');
 
     const { url, method } = request;
 
-    if(url === '/'){
-        if(method === 'GET'){
-            response.end(`<h1>Ini adalah homepage</h1>`)
-        }else(
-            response.end(`Halaman ini tidak dapat diakses dengan ${method} request`)
-        )
-    }
-    
-    if(url === '/about'){
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({message: `Ini adalah homepage` }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({message: `Halaman tidak dapat diakses dengan ${method} request` }));
+        }
+    } else if (url === '/about') {
         let body = []
-
-        request.on('data', (chunck)=>{
-            body.push(chunck)
+        request.on('data', (chunck) => {
+            body.push(chunck);
         });
 
-        if(method === 'GET'){
-            response.end(`<h1>Halo! Ini adalah halaman about</h1>`)
-        }else if(method === 'POST'){
-            request.on('end', ()=> {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({message: `Halo! Ini adalah halaman about` }));
+        } else if (method === 'POST') {
+            response.statusCode = 200;
+
+            request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
-                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`)
-            })
-        }else{
-            response.end(`Halaman ini tidak dapat diakses dengan ${method} request`)
+                response.end(JSON.stringify({message: `Halo, ${name}! Ini adalah halaman about` }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({message: `Halaman tidak dapat diakses dengan ${method} request` }));
         }
-
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({message: `Halaman tidak ditemukan` }));
     }
 };
 
 const server = http.createServer(requestListener)
 
-const port = 5010;
+const port = 5000;
 const host = 'localhost'
 
 server.listen(port, host, () => {
